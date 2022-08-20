7c26fc060233c41682b7b37b9c546b4bb572145c
theOtherMk <67931479+TheOtherMK@users.noreply.github.com>
2022-08-20 17:07:37 +0100

    draft created

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 
+let userSelection;
+let computerSelection;
+let result;
+
+
+
+
+function getComputerChoice () {
+
+    switch ((Math.floor(Math.random() * 3))) {
+
+        case 0:
+            computerSelection = 'Rock';
+            break;
+        case 1:
+            computerSelection = 'Paper';
+            break;
+        default:
+            computerSelection = 'Scissors'
+    }
+
+    return computerSelection;
+ 
+}
+
+console.log('computer choice is' + getComputerChoice())
+
+function getUserChoice() {
+
+    let userSelection = prompt('Enter rock, paper or scissors!');
+    userSelection = userSelection.toLowerCase();
+
+    switch(userSelection){
+        case rock:
+            text = 'Rock';
+            break;
+        case paper:
+            text = 'Paper';
+            break;
+        case scissors:
+            text = 'Scissors';
+            break;
+        default:
+            text= 'This is not a valid entry!';
+    }
+    document.getElementById("mainBox").innerHTML = text
+    return userSelection;
+}
+
+console.log('User choice is' + getUserChoice());
+
+
+
